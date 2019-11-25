94ac95dc62bebe27fbc2d62db5b85e043713e430
Jamil Alisgandarov <jamil.alisgandarov@pashabank.az>
2019-11-25 16:41:35 +0400

    Fixed tempalate path error

--- application/routes/asan/index.ts
+++ application/routes/asan/index.ts
@@ -8,7 +8,7 @@ ASAN_ROUTER.post('/asan-finance', (req, res) => {
     const body: IAsanFinanceReqParams = req.body;
 
     PDFGeneratorInstance
-        .createPdf('/asan/asan_finance,ejs', body)
+        .createPdf('/asan/asan_finance.ejs', body)
         .then((pdf) => {
             res.setHeader('Content-disposition', 'attachment; filename=agreement.pdf');
             res.end(pdf);
