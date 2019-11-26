e0566dc3a988dff183ba55a09474e19240b36bde
Jamil Alisgandarov <jamil.alisgandarov@pashabank.az>
2019-11-26 18:36:39 +0400

    Included `cuid` to give uinque name for files.

--- application/routes/index.ts
+++ application/routes/index.ts
@@ -7,7 +7,7 @@ export const BASE_ROUTER = Router();
     {
         path: '/asan',
         router: ASAN_ROUTER,
-    },
+    }
 ].forEach(route => {
     BASE_ROUTER.use(route.path, route.router);
 });
